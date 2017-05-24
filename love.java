99dbd201d44fa86b58c3aaa23725f4a15e872f73
harvansh7 <harvanshrajput7@gmail.com>
2017-05-24 12:06:50 +0530

    .java and .class files add

---
+++ love.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class LOVE {
+public static void main (String args[]) {
+Scanner HEART = new Scanner (System.in);
+int x, y, z;
+System.out.println ("Enter first number");
+x = HEART.nextInt();
+System.out.println ("Enter second number");
+y = HEART.nextInt();
+z = x + y;
+System.out.println ( z );
+}
+}
