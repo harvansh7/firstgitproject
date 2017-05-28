ce4be78feac65c3ac30568df7d0889ae751f0951
harvansh7 <harvanshrajput7@gmail.com>
2017-05-29 01:48:49 +0530

    finnaly  files commited

---
+++ AddTwoArray.java
@@ -0,0 +1,31 @@
+class removeDup{
+
+	public static void removeDupNumbers(int a[]){
+     
+     int i=0, j=0;
+while(i<a.length){
+     if( a[i] == a[j]){
+           j++;
+       }else{
+       	  i++;
+       	  a[i] = a[j];
+       	  j++;
+       }
+     }
+     for(int aa : a)
+        	System.out.println(aa);
+
+	}
+
+	public static void main(String arr[]){
+
+		int array[] = {1,1,2,3,4};
+       removeDupNumbers(array);
+       // System.out.println(x);
+
+        
+
+	}
+
+
+}
